44a856006d65dcbc5271b7feafb289f2ab2d3d74
Gaojy <340067958@qq.com>
2018-10-23 15:33:52 +0800

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -125,6 +125,27 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
     }
 
+    /**
+     * 关注列表
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(15);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(15);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+
+
 
 
 }
